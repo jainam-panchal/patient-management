89d094ff4569226d7f07693f7a513b96fde74ae2
Jainam Panchal <jainampanchal1@gmail.com>
2025-04-13 15:21:42 +0530

    gRPC integration into PatientService

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,12 +1,15 @@
 package com.pm.patientservice.service;
 
+import billing.BillingResponse;
 import com.pm.patientservice.dto.PatientRequestDTO;
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.exception.EmailAlreadyExistsException;
 import com.pm.patientservice.exception.PatientNotFoundException;
+import com.pm.patientservice.grpc.BillingServiceGrpcClient;
 import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +18,23 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class PatientService {
 
-    @Autowired
-    private PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
+    private final PatientRepository patientRepository;
+    private final PatientMapper patientMapper;
 
-    @Autowired
-    private PatientMapper patientMapper;
+    public PatientService(
+            BillingServiceGrpcClient billingServiceGrpcClient,
+            PatientRepository patientRepository,
+            PatientMapper patientMapper
+    ) {
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
+        this.patientRepository = patientRepository;
+        this.patientMapper = patientMapper;
+    }
 
     public List<PatientResponseDTO> getAllPatients() {
         List<Patient> patients = patientRepository.findAll();
@@ -40,7 +52,15 @@ public class PatientService {
         if(patientRepository.existsByEmail(patient.getEmail()))
             throw new EmailAlreadyExistsException("Patient with " + patient.getEmail() + " already exists");
 
-        patientRepository.save(patient);
+        Patient savedPatient = patientRepository.save(patient);
+
+        BillingResponse billingResponse = billingServiceGrpcClient.createBillingAccount(
+                savedPatient.getId().toString(),
+                savedPatient.getName(),
+                savedPatient.getEmail()
+        );
+        log.info("Created new billing account for patient {}", savedPatient.getId());
+
         return patientMapper.patientToPatientResponseDTO(patient);
     }
 
